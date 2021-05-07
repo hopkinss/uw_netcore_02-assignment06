ecd91274d97fe27ade2ef1c3fcebc102a167a32e
Shawn Hopkins <shopkins@seagen.com>
2021-05-07 09:38:08 -0700

    Inital commit for homework 6, lesson09

---
+++ BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BirthdayCard.Models;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BdayCard bdayCard;
+        public HomeController()
+        {
+            bdayCard = new BdayCard(); 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(BdayCard card)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("SendCard",card);                
+            }
+            return View(card);
+        }
+
+        public IActionResult SendCard(BdayCard card)
+        {
+
+            return View(card);
+        }
+
+
+    }
+}
---
+++ BirthdayCard/Models/BdayCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayCard.Models
+{
+    public class BdayCard
+    {
+        [Required(ErrorMessage = "Please enter To")]
+        [Display(Name = "To:")]
+        [StringLength(50)]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter From")]
+        [Display(Name = "From:")]
+        [StringLength(50)]
+        public string From  { get; set; }
+
+        [Required(ErrorMessage = "Please enter Message")]
+        [Display(Name = "Message:")]
+        [StringLength(150)]
+        public string Msg { get; set; }
+
+        public string SentMessage { get; set; }
+    }
+}
